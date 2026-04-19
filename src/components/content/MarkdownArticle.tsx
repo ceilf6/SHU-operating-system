1a5f823391c45d4e8bd50c2cc69a46fe02957be4
WJH <3506456886@qq.com>
2026-04-19 18:32:18 +0800

    feat: 解析 md 中图片 - 如果写以 / 开头的图片路径，会自动带上站点的 BASE_URL

--- src/components/content/MarkdownArticle.tsx
+++ src/components/content/MarkdownArticle.tsx
@@ -5,10 +5,30 @@ interface MarkdownArticleProps {
   content: string;
 }
 
+function resolveAssetUrl(src?: string) {
+  if (!src) {
+    return "";
+  }
+  if (/^(https?:)?\/\//.test(src) || src.startsWith("data:")) {
+    return src;
+  }
+  if (src.startsWith("/")) {
+    return `${import.meta.env.BASE_URL}${src.replace(/^\/+/, "")}`;
+  }
+  return src;
+}
+
 export function MarkdownArticle({ content }: MarkdownArticleProps) {
   return (
     <div className="markdown-block">
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          img: ({ src, alt }) => <img src={resolveAssetUrl(src)} alt={alt ?? ""} loading="lazy" />,
+        }}
+      >
+        {content}
+      </ReactMarkdown>
     </div>
   );
 }
